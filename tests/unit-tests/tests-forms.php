0dbcc1047ad75e37bccc3aec5dfbf24fd5406772
David Cramer <David Cramer>
2016-02-04 14:42:50 +0200

    test test test

--- tests/unit-tests/tests-forms.php
+++ tests/unit-tests/tests-forms.php
@@ -8,8 +8,12 @@ class Tests_Forms extends CF_API_Unit_Test_Case {
 
 	public function setUp() {
 		parent::setUp();
-				
+		
 	}
-
+	
+	public function testSample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
 }
 
