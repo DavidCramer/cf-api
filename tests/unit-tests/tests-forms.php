6adf6b81eaa096e0d5aaabe68ff2fbe53905456e
David Cramer <David Cramer>
2016-02-04 14:34:49 +0200

    clear out setup

--- tests/unit-tests/tests-forms.php
+++ tests/unit-tests/tests-forms.php
@@ -8,12 +8,7 @@ class Tests_Forms extends CF_API_Unit_Test_Case {
 
 	public function setUp() {
 		parent::setUp();
-
-		global $wp_rewrite, $wp_query;
-		$GLOBALS['wp_rewrite']->init();
-		flush_rewrite_rules( false );
-
-		
+				
 	}
 
 }
